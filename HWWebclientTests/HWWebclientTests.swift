b80607b0ebd4b5a323a203d42242de3f73cbbd6c
Andrey Ostroverkhiy <a.ostroverkhiy@mobidev.biz>
2019-03-26 17:04:05 +0200

    some tests added (draft)

--- HWWebclientTests/HWWebclientTests.swift
+++ HWWebclientTests/HWWebclientTests.swift
@@ -19,9 +19,52 @@ class HWWebclientTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+	func testTest() {
+		print("✅")
+	}
+
+    func testGet() {
+
+
+		class TestConfig: HWWebClientConfigInfo {}
+		class TestModel: Codable {
+			let id, userId: Int
+			let title: String
+			let completed: Bool
+		}
+
+		class TestServiceProvider {
+			let url = "https://jsщnplaceholder.typicode.com/todos/1"
+			let wc = HWWebClient(TestConfig())
+
+			func testService(onSuccess: @escaping ((TestModel)->Void),
+							 onError: ((HWWebClientError) -> Void)?) {
+
+				wc.get(url, success: { (response) in
+					_ = self.wc.handleResponseUnwrapped(response,
+														modelType: TestModel.self,
+														successHandler: onSuccess,
+														failureHandler: onError)
+				}, failure: onError)
+
+			}
+		}
+
+
+
+		TestServiceProvider().testService(onSuccess: { (model) in
+
+			let isSuccess = model.id == 1
+				&& model.userId == 1
+				&& model.title == "delectus aut autem"
+				&& model.completed == false
+
+			print(isSuccess ? "✅" : "❌")
+			XCTAssert(isSuccess, "Failed to load test model")
+		}) { (error) in
+			print("❌: \(error)")
+			XCTAssert(false, "Failed to load test model with error: \(error)")
+		}
     }
 
     func testPerformanceExample() {
